339d54be94286cf5ff2c315913d037a99b67e98c
Luis Lacroix <128614343+Masterchief2074@users.noreply.github.com>
2023-05-23 09:09:47 -0600

    Actualizacion suma, resta, multiplicacion y division

---
+++ js/main.js
@@ -0,0 +1,28 @@
+ console.log("Hello World")
+ 
+ //alert("Bienvenido a Java Script")
+
+ /*
+ Comentario multilinea
+ */
+
+ //let nombre = prompt("Cual es tu  nombre?");
+
+ //console.log(parseInt(nombre))
+
+ //console.log(typeof(nombre))
+
+
+ let numeroUno = prompt("Ingresa un numero");
+ let numeroDos = prompt("Ingresa otro numero");
+
+ let resultado = parseInt(numeroUno) + parseInt(numeroDos);
+ 
+ let resultado = parseInt(numeroUno) - parseInt(numeroDos);
+ 
+ let resultado = parseInt(numeroUno) * parseInt(numeroDos);
+ 
+ let resultado = parseInt(numeroUno) / parseInt(numeroDos);
+ 
+ console.log(`El resultado de la suma es: ${resultado}`);
+
