2f7b3646ede9c3b563ae0ce32405fc4db6b9814e
Luis Lacroix <128614343+Masterchief2074@users.noreply.github.com>
2023-05-24 09:10:14 -0600

    Condicionales

--- js/main.js
+++ js/main.js
@@ -16,13 +16,36 @@
  let numeroUno = prompt("Ingresa un numero");
  let numeroDos = prompt("Ingresa otro numero");
 
- let resultado = parseInt(numeroUno) + parseInt(numeroDos);
+ let suma = parseInt(numeroUno) + parseInt(numeroDos);
  
- let resultado = parseInt(numeroUno) - parseInt(numeroDos);
+ let resta = parseInt(numeroUno) - parseInt(numeroDos);
  
- let resultado = parseInt(numeroUno) * parseInt(numeroDos);
+ let multi = parseInt(numeroUno) * parseInt(numeroDos);
  
- let resultado = parseInt(numeroUno) / parseInt(numeroDos);
+ let division = parseInt(numeroUno) / parseInt(numeroDos);
  
  console.log(`El resultado de la suma es: ${resultado}`);
+
+
+ //Dados dos numerosidentificar cual numero es mayor,
+ //cual es menor o si son iguales.
+ 
+ let primerNumero = prompt("Ingresa un numero");
+ let segundoNumero = prompt("Ingresa otro numero");
+
+ if(primerNumero > segundoNumero){
+   console.log(`El numero $ {primerNumero} es ,meyor que $ {segundoNumero}`);
+}else if (segundoNumero > primerNumero){
+    console.log(`El numero $ {segundoNumero} es mayor que $ {primerNumero}`);
+}else{
+    console.log(`El numero $ {primerNumero} es ,meyor que $ {sedundoNumero}`);
+}
+
+//Dado un numero, identificar sim es numero par o impar.
+
+let numero= prompt("Ingresa un numero");
+
+
+
+
 
